6cd4a789bb66aa42d1d5854e8f8c88e2c068379d
Anjo Tadena <anjo@coderepubliq.com>
2018-03-19 05:13:18 +0800

    Simplfy fizzbuzz algorithm.

--- app.js
+++ app.js
@@ -1,58 +1,32 @@
 
-// Fizzbuzz
+// Constants
 var constant = {
+	// FizzBuzz
 	fizz: 'Fizz',
 	buzz: 'Buzz',
 };
 
-// function fizzBuzz(num) {
-	
-// 	for (var i = 1; i <= num; i++) {
+/**
+ * [fizzBuzz description]
+ * @param  {[type]} num [description]
+ * @return {[type]}     [description]
+ */
+function fizzBuzz(num) {
+
+	for (var i = 1; i <= num; i++) {
 		
-// 		// i % 3 === 0 && i % 5 === 0
-// 		if (i % 15 === 0) {
-// 			console.log(constant.fizz + constant.buzz);
-// 		} else if (i % 3 === 0) {
-// 			console.log(constant.fizz);
-// 		} else if (i % 5 === 0) {
-// 			console.log(constant.buzz);
-// 		} else {
-// 			console.log(i);
-// 		}
-// 	}
-// }
-
-// Recursion
-function fizzBuzz(start, num) {
-	debugger;
-	if (start > num) {
-		return;
+		// i % 3 === 0 && i % 5 === 0
+		if (i % 15 === 0) {
+			console.log(constant.fizz + constant.buzz);
+		} else if (i % 3 === 0) {
+			console.log(constant.fizz);
+		} else if (i % 5 === 0) {
+			console.log(constant.buzz);
+		} else {
+			console.log(i);
+		}
 	}
-
-	// i % 3 === 0 && i % 5 === 0
-	if (start % 15 === 0) {
-
-		console.log(constant.fizz + constant.buzz);
-
-		return fizzBuzz(++start, num);
-	}
-
-	if (start % 3 === 0) {
-		
-		console.log(constant.fizz);
-
-		return fizzBuzz(++start, num);
-	}
-
-	if (start % 5 === 0) {
-
-		console.log(constant.buzz);
-
-		return fizzBuzz(++start, num);
-	}
-
-	console.log(start);
-	return fizzBuzz(++start, num);
 }
 
+// Fizzbuzz
 fizzBuzz(1, 20);
